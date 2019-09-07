0cd003ecd9932d01feae005d6502eea151550738
srn11 <srnandhini1108@gmail.com>
2019-09-08 00:53:14 +0530

    Folders added

---
+++ javascript/popupModal.js
@@ -0,0 +1,6 @@
+function openPop(){
+      document.getElementById('modal').style.visibility = "visible";
+    }
+function exit(){
+    document.getElementById('modal').style.visibility = "hidden";
+  }
